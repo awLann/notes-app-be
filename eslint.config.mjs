ddcca9edcb8d61e0fa5a0caceffbd791542fa7ce
awLann <alwanfajrul20@gmail.com>
2024-11-25 15:50:55 +0700

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import daStyle from "eslint-config-dicodingacademy";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: globals.node }},
+  pluginJs.configs.recommended,
+];
